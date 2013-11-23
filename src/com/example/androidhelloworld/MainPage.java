202227f3a23bb959c166f06b93b6455f11bc8d7a
Jonathan Lei <jonathan.lei01@gmail.com>
2013-11-22 16:07:30 -0800

    first commit

---
+++ src/com/example/androidhelloworld/MainPage.java
@@ -0,0 +1,53 @@
+package com.example.androidhelloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainPage extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        
+        Button uselessButton = (Button)findViewById(R.id.button2);
+        uselessButton.setOnClickListener(new OnClickListener(){
+        	
+        	@Override
+			public void onClick(View v) {
+				TextView title = (TextView)findViewById(R.id.helloWorld);
+				title.setText("My First App");
+				
+			}
+			
+        });
+        
+    }
+    
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Hello_Name.class);
+        EditText editText = (EditText) findViewById(R.id.nameHere);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_page, menu);
+        return true;
+    }
+    
+}
